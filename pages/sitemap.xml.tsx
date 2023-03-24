b7c991f0e0e5ccbd9497f5d4258f56e2a70a0be9
Denys <denys.kravchenko@sl.nsw.gov.au>
2023-03-24 21:36:47 +1100

    fix

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,43 +1,31 @@
 import { GetServerSideProps } from "next";
-// import fetch from "isomorphic-unfetch";
 
-// const EXTERNAL_DATA_URL = 'https://memento-mori-psi.vercel.app/posts';
-
-function generateSiteMap(posts: any[]) {
+const generateSiteMap = () => {
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://memento-mori-psi.vercel.app</loc>
-     </url>
-     <url>
-       <loc>https://memento-mori-psi.vercel.app/about</loc>
-     </url>
+     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <!--We manually set the two URLs we know already-->
+       <url>
+         <loc>https://memento-mori-psi.vercel.app</loc>
+       </url>
+       <url>
+         <loc>https://memento-mori-psi.vercel.app/about</loc>
+       </url>
+     </urlset>
+   `;
+};
 
-   </urlset>
- `;
-}
-
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
-}
+const SiteMap = () => {
+  return null; // render nothing for this component
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  //   // We make an API call to gather the URLs for our site
-  //   const request = await fetch(EXTERNAL_DATA_URL);
-  //   const posts = await request.json();
+  const sitemap = generateSiteMap();
 
-  //   // We generate the XML sitemap with the posts data
-  //   const sitemap = generateSiteMap(posts);
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
 
-  //   res.setHeader('Content-Type', 'text/xml');
-  //   // we send the XML to the browser
-  //   res.write(sitemap);
-  //   res.end();
-
-  return {
-    props: {},
-  };
+  return { props: {} };
 };
 
 export default SiteMap;
